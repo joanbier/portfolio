afaafb5dec642bc8e39d7e50328d393dc8619222
jbiernat <joanna.biernat@upbonus.pl>
2025-07-29 18:08:55 +0200

    translation contact & footer fix

--- src/app/components/project/project.ts
+++ src/app/components/project/project.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { SectionTitle } from '../../shared/section-title/section-title';
 import { FormsModule } from '@angular/forms';
 import { Btn } from '../../shared/btn/btn';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-project',
-  imports: [SectionTitle, FormsModule, Btn],
+  imports: [SectionTitle, FormsModule, Btn, TranslatePipe],
   templateUrl: './project.html',
   styleUrl: './project.scss',
 })
