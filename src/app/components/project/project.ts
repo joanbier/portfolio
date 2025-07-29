8ff7c96b85812ebc9cb00edb190ea54c77e9398a
jbiernat <joanna.biernat@upbonus.pl>
2025-07-29 20:10:46 +0200

    translation project section

--- src/app/components/project/project.ts
+++ src/app/components/project/project.ts
@@ -3,10 +3,19 @@ import { SectionTitle } from '../../shared/section-title/section-title';
 import { FormsModule } from '@angular/forms';
 import { Btn } from '../../shared/btn/btn';
 import { TranslatePipe } from '@ngx-translate/core';
+import { AsyncPipe } from '@angular/common';
+import { TranslateSafeHtmlPipe } from '../../shared/translate-safe-html-pipe';
 
 @Component({
   selector: 'app-project',
-  imports: [SectionTitle, FormsModule, Btn, TranslatePipe],
+  imports: [
+    SectionTitle,
+    FormsModule,
+    Btn,
+    TranslatePipe,
+    AsyncPipe,
+    TranslateSafeHtmlPipe,
+  ],
   templateUrl: './project.html',
   styleUrl: './project.scss',
 })
