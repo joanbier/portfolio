ae1e466a673d48697236c0468946bb47dd317c1f
jbiernat <joanna.biernat@upbonus.pl>
2025-07-29 14:16:51 +0200

    lang-switch cleaning

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,16 +7,14 @@ import { Skills } from './components/skills/skills';
 import { Contact } from './components/contact/contact';
 import { Footer } from './components/footer/footer';
 import { TranslateService } from '@ngx-translate/core';
-import { TestNav } from './components/test-nav/test-nav';
 
 @Component({
   selector: 'app-root',
   styles: [],
-  imports: [Nav, Hero, About, Project, Skills, Contact, Footer, TestNav],
+  imports: [Nav, Hero, About, Project, Skills, Contact, Footer],
   template: `
     <header id="hero">
       <app-nav />
-      <!--      <app-test-nav />-->
       <app-hero />
     </header>
     <main>
